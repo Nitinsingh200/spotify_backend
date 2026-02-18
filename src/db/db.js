f6842179967b5c83e249c5f59859780bf48515f3
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-18 19:44:09 +0530

    first

---
+++ src/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+ async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("database connect successfully")
+    }
+    catch(err){
+        console.log("database connection error", err);
+
+    }
+ }
+
+ module.exports =connectDB;
+ 
