4fc237269a24243d77a43780be0ede23a07a5806
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-19 19:27:16 +0530

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,4 +45,39 @@ async function registerUser( req,res){
 
 
 }
-module.exports ={registerUser}
+async function loginUser(req,res){
+     const {username, email,password}=req.body;
+     const user = await  userModel.findOne({
+          $or:[
+               {username},
+               {email}
+          ]
+     })
+if(!user){
+     return res.status(401).json({message:"invalid credential"})
+}
+
+ const isPasswordValid = await bcrypt.compare(password,user.password)
+ if(!isPasswordValid){
+     return res.status(401).json({message:"Invalid credential"})
+ }
+  const token = jwt.sign({
+     id:user._id,
+     role:user.role,
+
+
+  },process.env.JWT_SECRET)
+ 
+  res.cookie("token",token)
+
+  res.status(200).json({
+     message:"user logged in successfully",
+     user:{
+          id:user._id,
+          username:user.username,
+          email:user.email,
+          role:user.role,
+     }
+  })
+}
+module.exports ={registerUser,loginUser}
