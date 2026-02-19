ace2b002864b690fa53c493e20d5a1b4684d4e02
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-19 18:29:42 +0530

    first

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,45 @@
+const userModel = require("../model/user.model")
+
+ const   jwt = require("jsonwebtoken")
+async function registerUser( req,res){
+     const {username,email,password,role="user"}= req.body;
+
+     const  isUserAlreadyExists = await   userModel.findOne({
+          $or:[
+               {username},
+               {email}
+
+          ]
+     }) 
+     if(isUserAlreadyExists){
+            return res.status(409).json({message:"user already  exist "})
+     }
+     
+      const user = await userModel.create({
+          username,
+          email,
+          password,
+          role
+      })
+
+
+      const token = jwt.sign({
+          id:user._id,
+          role:user.role,
+
+      },process.env.JWT_SECRET)
+
+      res.cookie("token",token)
+
+      res.status(201).json({
+          message:"user register successfully",
+          user:{
+               id:user._id,
+               username:user.username,
+               email:user.email,
+               role:user.role,
+          }
+      })
+
+
+}
