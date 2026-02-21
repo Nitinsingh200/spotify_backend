6f54bfe93e5f1858a80c95c3db99229c6a82560b
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-02-21 21:55:07 +0530

    create

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -17,6 +17,25 @@ return res.status(401).json({message:"unauthorized"})
     }
      const {title} = req.body;
      const file = req.file
-      const result =await uploadFile(file)
+      const result =await uploadFile(file.buffer.toString('base64'))
+
+      const  music = await musicModel.create({
+        uri:result.url,
+        title,
+        artist:decoded.id
+         
+      })
+      res,status(201).json({
+         message:" music created successfully",
+         music:{
+            id:music._id,
+            uri:music.uri,
+            title:music.title,
+            artist:music.artist,
+         }
+      })
+
 
 }
+
+module.exports ={createMusic}
